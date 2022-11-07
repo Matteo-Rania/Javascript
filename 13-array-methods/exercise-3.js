db21cf2a55f4549722f6afc87af98d19c6c39b7d
Matteo Rania <matteo.rania2014@gmail.com>
2022-11-07 14:26:54 +0100

    13-array-methods

---
+++ 13-array-methods/exercise-3.js
@@ -0,0 +1,21 @@
+function calculateAverageAge(persons) {
+  return Math.round(persons.reduce((total, item) =>
+  ((total + item.age)),0)/ (persons.length));
+}
+
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
